20417cf139c24b750cb4e82e3d91919779e9775a
Eric Wang <johnericcw@gmail.com>
2017-11-11 09:26:18 -0600

    Correctly injecting script and logging video title/ uploader

--- main.js
+++ main.js
@@ -1,10 +1,52 @@
-console.log("hello world!");
+"use strict";
 
-var n = {
-	type: "basic",
-	iconUrl: "logo.png",
-	title: "This...",
-	message: "...is a test"
-};
+// these selectors will likely change as YouTube changes its interface
+const VIDEO_TITLE_SELECTOR = "#info-contents .title";
+const VIDEO_UPLOADER_SELECTOR = "#meta-contents #owner-name";
 
-setInterval(function() {chrome.notifications.create(n)}, 5000);
+function log(text) {
+	console.log(`[YTN] ${text}`);
+}
+
+function getSingleMatchingNodeText(selector) {
+	const matchingNodes = document.querySelectorAll(selector);
+	if (matchingNodes.length != 1) {
+		throw `Number of nodes matching "${selector}" is not equal to 1 (${matchingNodes.length})`;
+	}
+
+	return matchingNodes[0].textContent;
+}
+
+function getVideoTitle() {
+	return getSingleMatchingNodeText(VIDEO_TITLE_SELECTOR);
+}
+
+function getVideoUploader() {
+	return getSingleMatchingNodeText(VIDEO_UPLOADER_SELECTOR);
+}
+
+function main() {
+	console.log("[YTN] Successfully loaded YoutubeNotifications.");
+
+	setInterval(function() { 
+		log(getVideoTitle()); 
+	}, 1000);
+
+	setInterval(function() {
+		log(getVideoUploader());
+	}, 1000);
+
+	// TODO: move to a file that can actually create the notification
+	// console.log("hello world!");
+
+	// var n = {
+	// 	type: "basic",
+	// 	iconUrl: "logo.png",
+	// 	title: "This...",
+	// 	message: "...is a test"
+	// };
+
+	// chrome.notifications.create(n);
+}
+
+main();
